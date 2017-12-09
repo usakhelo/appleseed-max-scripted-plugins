7bfdde4cc0e813efed509b828b9b546f9641d15b
Sergo <usakhelo@users.noreply.github.com>
2017-12-09 01:00:04 -0500

    Some progress

--- src/appleseed-max-impl/appleseedosltexture/oslshaderregistry.cpp
+++ src/appleseed-max-impl/appleseedosltexture/oslshaderregistry.cpp
@@ -11,10 +11,14 @@
 
 namespace
 {
-
-
     ShaderInfo shader_1;
     ShaderInfo shader_2;
+
+    int numID = 100;
+    int ctrlID() 
+    { 
+        return numID++;
+    }
 }
 
 void OSLShaderRegistry::instanciate_shader_plugins()
@@ -41,36 +45,47 @@ void OSLShaderRegistry::instanciate_shader_plugins()
                 L"oslTextureMapParams",                     // internal parameter block's name
                 0,                                          // ID of the localized name string
                 class_descr.Get(),                          // class descriptor
-                P_AUTO_CONSTRUCT, // + P_AUTO_UI,                           // block flags
+                P_AUTO_CONSTRUCT,// + P_AUTO_UI,                           // block flags
 
                                                             // --- P_AUTO_CONSTRUCT arguments ---
                 0,                                          // parameter block's reference number
 
-                //IDD_PANEL, IDS_PARAMS, 0, 0, NULL,
-                //0,                   /* Parameter ID. We are defining the first parameter here */
-                //L"p0",              /* Internal name of the parameter */
-                //TYPE_FLOAT,          /* Parameter Type. It will be a float parameter */
-                //P_ANIMATABLE,        /* A constant defined in iparamb2.h. Indicates that the parameter is animatable.*/
-                //p_end,               /* End of the first parameter definition.*/
-                //1,                   /* Parameter ID. This will be the second parameter*/
-                //L"p1",                  /* Internal name of the parameter*/
-                //TYPE_FLOAT,          /* Parameter Type. It will be a float parameter*/
-                //P_ANIMATABLE,        /* A constant defined in iparamb2.h. Indicates that the parameter is animatable.*/
-                //p_end,               /* End of the second parameter definition. 'end' is an enumerated value defined in
+                // --- P_AUTO_UI arguments for rollup ---
+                //0, //ParamMapIdDisney,
+                //0, //IDD_FORMVIEW_PARAMS,                        // ID of the dialog template
+                //0, //IDS_FORMVIEW_PARAMS_TITLE,                  // ID of the dialog's title string
+                //0,                                          // IParamMap2 creation/deletion flag mask
+                //0,                                          // rollup creation flag
+                //nullptr,                                    // user dialog procedure
+
+                0,                   /* Parameter ID. We are defining the first parameter here */
+                L"p0",              /* Internal name of the parameter */
+                TYPE_RGBA,          /* Parameter Type. It will be a float parameter */
+                P_ANIMATABLE,        /* A constant defined in iparamb2.h. Indicates that the parameter is animatable.*/
+                0,                  //string table id, e.g. IDS_BASE_COLOR
+                p_ui, TYPE_COLORSWATCH, ctrlID(),
+                p_end,               /* End of the first parameter definition.*/
+                1,                   /* Parameter ID. This will be the second parameter*/
+                L"p1",                  /* Internal name of the parameter*/
+                TYPE_FLOAT,          /* Parameter Type. It will be a float parameter*/
+                P_ANIMATABLE,        /* A constant defined in iparamb2.h. Indicates that the parameter is animatable.*/
+                0,                  //string table id, e.g. IDS_BASE_COLOR
+                p_ui, TYPE_SLIDER, EDITTYPE_FLOAT, ctrlID(), ctrlID(), 10.0f,
+                p_end,               /* End of the second parameter definition. 'end' is an enumerated value defined in
                                      /* the 'ParamTags' enumerator.*/
 
             p_end
             ));
 
         //param_block_descr->SetClassDesc(class_descr.Get());
-        param_block_descr->AddParam(
-            0,                   /* Parameter ID. We are defining the first parameter here */
-            _M("p0"),              /* Internal name of the parameter */
-            TYPE_FLOAT,          /* Parameter Type. It will be a float parameter */
-            P_ANIMATABLE,        /* A constant defined in iparamb2.h. Indicates that the parameter is animatable.*/
-            0,                  //string table id, e.g. IDS_BASE_COLOR
-            p_end               /* End of the first parameter definition.*/
-        );
+        //param_block_descr->AddParam(
+        //    0,                   /* Parameter ID. We are defining the first parameter here */
+        //    _M("p0"),              /* Internal name of the parameter */
+        //    TYPE_FLOAT,          /* Parameter Type. It will be a float parameter */
+        //    P_ANIMATABLE,        /* A constant defined in iparamb2.h. Indicates that the parameter is animatable.*/
+        //    0,                  //string table id, e.g. IDS_BASE_COLOR
+        //    p_end               /* End of the first parameter definition.*/
+        //);
         m_paramblock_descriptors.push_back(MaxSDK::AutoPtr<ParamBlockDesc2>(param_block_descr));
         m_class_descriptors.push_back(MaxSDK::AutoPtr<ClassDesc2>(class_descr));
     }
